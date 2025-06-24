7ebffd6fedfe6762bcc5b50cde2cb7d6181b556a
Deniz Baykal <denizbaykal2006@outlook.com>
2025-06-24 14:03:00 +0300

    İlk commit

---
+++ Simple.py
@@ -0,0 +1,20 @@
+from statistics import mean
+
+num_list = list()
+while True:
+    try:
+        num = input("Enter a number: ")
+        if num == "done":
+            break
+        value = float(num)
+        num_list.append(value)
+    except Exception:
+        print("Please enter a real number!")
+
+try:
+    max_num = max(num_list)
+    min_num = min(num_list)
+    avr_num = mean(num_list)
+    print(f"Biggest number: {max_num}, Smallest number: {min_num}, Average: {avr_num}")
+except Exception:
+    print("Error! Please make sure you are putting a number.")
